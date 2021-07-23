42c1820679c67800b7bc4bba3f9a95f534ce94e1
carlos.anguila <canguila@deloitte.com>
2021-07-23 18:00:44 -0500

    Added "tus favoritos" section

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,11 +1,43 @@
 import React from 'react';
 import './menu.css';
 import {AddDish} from "../Dish";
-import {Row} from "react-bootstrap";
+import {Card, Row} from "react-bootstrap";
 
 export const Menu = () => {
     return (
         <>
+            <Row>
+                <div className={"col-md-8 mt-3 label-bold"}>Tus favoritos</div>
+            </Row>
+            <Row>
+                <Card style={{width: '18rem'}}>
+                    <Card.Img variant="top" src="https://via.placeholder.com/286x160.png"/>
+                    <Card.Body>
+                        <Card.Title>Veggie Avocado</Card.Title>
+                        <Card.Text>
+                            $310
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                <Card style={{width: '18rem'}}>
+                    <Card.Img variant="top" src="https://via.placeholder.com/286x160.png"/>
+                    <Card.Body>
+                        <Card.Title>Veggie Avocado</Card.Title>
+                        <Card.Text>
+                            $310
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+                <Card style={{width: '18rem'}}>
+                    <Card.Img variant="top" src="https://via.placeholder.com/286x160.png"/>
+                    <Card.Body>
+                        <Card.Title>Veggie Avocado</Card.Title>
+                        <Card.Text>
+                            $310
+                        </Card.Text>
+                    </Card.Body>
+                </Card>
+            </Row>
             <Row>
                 <div className={"col-md-8 mt-3 label-bold"}>Nuestros platos</div>
             </Row>
